afad41974d9dc7982dda3588d495437461658a04
Lis <lisleitora@gmail.com>
2021-02-19 11:47:47 +0000

    first commit

---
+++ day2/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace day2
+{
+    public partial class MainPage : ContentPage
+    {
+        string amiga;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void _saveButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+            amiga = amiga+"\n"+_nameEntry.Text;
+            _textLabel.Text = amiga;
+
+        }
+    }
+}
+//    public partial class MainPage : ContentPage
+//    {
+//        string amiga;
+//        public MainPage()
+//        {
+//            InitializeComponent();
+//        }
+
+//        void _saveButton_Clicked(System.Object sender, System.EventArgs e)
+//        {
+//            amiga = amiga + "\n " + _nameEntry.Text;
+//            _textLabel.Text = amiga;
+
+//        }
+//    }
+//}
+
